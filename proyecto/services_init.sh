c30ce3ea6dff6109874935e92c0dfcd4fc5afd34
Gonzalo Salazar Muñoz <gonsalazarm@gmail.com>
2022-12-13 16:07:20 -0300

    añadido manejo de errores en bash

--- proyecto/services_init.sh
+++ proyecto/services_init.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
+function start_service {
+  while true; do
+    python3 $1 &
+    PID=$!
+    trap "kill $PID" SIGINT SIGTERM
+    if wait $PID; then
+      echo "$1 exited successfully"
+    else
+      echo "$1 exited with an error"
+    fi
+  done
+  exit 0
+}
+
 echo "================================"
 echo "Inicializando todos los servicios"
 echo "================================"
-python3 svc_db.py &
-python3 svc_login.py &
-python3 svc_usuarios.py &
-python3 svc_productos.py &
-python3 svc_alerta.py &
-python3 svc_confirmar.py
+
+start_service svc_db.py &
+start_service svc_login.py &
+start_service svc_usuarios.py &
+start_service svc_productos.py &
+start_service svc_alerta.py &
+start_service svc_confirmar.py
